618cacd85bf389b0f0588793290e3c370625949f
laurohen <laurohen@hotmail.com>
2020-01-27 00:57:24 -0300

    Commit start script-create-table-mysql

---
+++ script-create-table-mysql/netshowme.sql
@@ -0,0 +1,19 @@
+﻿#
+# Structure for table "lista_form_contatos"
+#
+
+DROP TABLE IF EXISTS `lista_form_contatos`;
+CREATE TABLE `lista_form_contatos` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mensagem` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `url_file` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ip_user` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `updated_at` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
